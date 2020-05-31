c891d4cb1b97a86aec13445a04853ad515b88627
Kenny Yang <kenny.j.yang@gmail.com>
2020-05-31 09:06:01 -0500

    update deploy files

--- sdgdemo-setup.sh
+++ sdgdemo-setup.sh
@@ -1,2 +1,7 @@
 oc new project sdgdemo
 oc -n <control plane project> patch --type='json' smmr default -p '[{"op": "add", "path": "/spec/members", "value":["'"sdgdemo"'"]}]'
+#deploy applications
+oc apply -n bookinfo -f https://raw.githubusercontent.com/Maistra/istio/maistra-1.1/samples/bookinfo/platform/kube/bookinfo.yaml
+oc apply -n bookinfo -f https://raw.githubusercontent.com/Maistra/istio/maistra-1.1/samples/bookinfo/platform/kube/bookinfo.yaml
+#update istio-ingress gateway
+oc apply -n bookinfo -f https://raw.githubusercontent.com/Maistra/istio/maistra-1.1/samples/bookinfo/networking/bookinfo-gateway.yaml
