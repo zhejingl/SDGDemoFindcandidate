ea2c920e77caff88292ad66d9ad56a26b0632da7
Kenny Yang <kenny.j.yang@gmail.com>
2020-05-31 14:06:15 -0500

    promote

--- sdgdemo-setup.sh
+++ sdgdemo-setup.sh
@@ -1,7 +1,20 @@
 oc new project sdgdemo
-oc -n <control plane project> patch --type='json' smmr default -p '[{"op": "add", "path": "/spec/members", "value":["'"sdgdemo"'"]}]'
+oc -n istio-system patch --type='json' smmr default -p '[{"op": "add", "path": "/spec/members", "value":["'"sdgdemo"'"]}]'
 #deploy applications
-oc apply -n bookinfo -f https://raw.githubusercontent.com/Maistra/istio/maistra-1.1/samples/bookinfo/platform/kube/bookinfo.yaml
-oc apply -n bookinfo -f https://raw.githubusercontent.com/Maistra/istio/maistra-1.1/samples/bookinfo/platform/kube/bookinfo.yaml
+#candidate
+oc apply -n sdgdemo -f https://raw.githubusercontent.com/zhejingl/SDGDemoFindcandidate/master/istio/deployment/deployment.yaml
+#titles
+oc apply -n sdgdemo -f https://raw.githubusercontent.com/zhejingl/SDGDemoBoot/master/deployment/deployment.yaml
+
+
 #update istio-ingress gateway
-oc apply -n bookinfo -f https://raw.githubusercontent.com/Maistra/istio/maistra-1.1/samples/bookinfo/networking/bookinfo-gateway.yaml
+oc apply -n sdgdemo -f https://raw.githubusercontent.com/zhejingl/SDGDemoFindcandidate/master/istio/networking/sdgcandidate-gateway.yaml
+
+export GATEWAY_URL=$(oc -n istio-system get route istio-ingressgateway -o jsonpath='{.spec.host}')
+
+#if mutual TLS is not enabled: 
+oc apply -n sdgdemo -f https://raw.githubusercontent.com/zhejingl/SDGDemoFindcandidate/master/istio/networking/destination-rull-all.yaml
+
+#if mutual TLS is enabled: 
+oc apply -n sdgdemo -f https://raw.githubusercontent.com/zhejingl/SDGDemoFindcandidate/master/istio/networking/destination-rule-all-mtls
+
--- src/main/java/sdg/demo/candidate/SDGCandidateController.java
+++ src/main/java/sdg/demo/candidate/SDGCandidateController.java
@@ -18,7 +18,7 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class SDGCandidateController {
-	private static final String RESPONSE_STRING_FORMAT = "customer => %s\n";
+	private static final String RESPONSE_STRING_FORMAT = "candidate => %s\n";
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
@@ -28,13 +28,31 @@ public class SDGCandidateController {
     @Autowired
     private Tracer tracer;
 	*/
-    @Value("${preferences.api.url:http://preference:8080}")
+    @Value("${preferences.api.url:http://titles:9080}")
     private String remoteURL;
     
     public SDGCandidateController(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
-
+    
+    @RequestMapping("/")
+    public ResponseEntity<?> getTitles() {
+        try {
+            // ResponseEntity<String> responseEntity = restTemplate.getForEntity(remoteURL, String.class);
+            String response = "Hitting candidate service";
+            return ResponseEntity.ok(String.format(RESPONSE_STRING_FORMAT, response.trim()));
+        } catch (HttpStatusCodeException ex) {
+            logger.warn("Exception trying to get the response from recommendation service.", ex);
+            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+                    .body(String.format(RESPONSE_STRING_FORMAT,
+                            String.format("%d %s", ex.getRawStatusCode(), createHttpErrorResponseString(ex))));
+        } catch (RestClientException ex) {
+            logger.warn("Exception trying to get the response from recommendation service.", ex);
+            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+                    .body(String.format(RESPONSE_STRING_FORMAT, ex.getMessage()));
+        }
+    }
+    
     @RequestMapping(value = "/v1/api/sdg/demo/candiate/{title}", method = RequestMethod.GET)
     public ResponseEntity<String> getCandidate(@RequestHeader("User-Agent") String userAgent, 
     											@RequestHeader(value = "user-preference", 		
